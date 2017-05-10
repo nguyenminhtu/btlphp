f1d2f11cfe8c20afd72a4514eaff57956841452a
Nguyễn Minh Tú <tuunguyen2795@gmail.com>
2017-05-10 17:27:33 +0700

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -118,8 +118,8 @@ function sendmail($receive, $body, $success) {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com;';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'tuunguyen2795@gmail.com';                 // SMTP username
-    $mail->Password = 'Tunguyen02071994';                           // SMTP password
+    $mail->Username = '';                 // SMTP username
+    $mail->Password = '';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 25;                                    // TCP port to connect to
 
